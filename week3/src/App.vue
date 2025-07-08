9a938f9159bb2354ea029eae562be869258f7e87
xcao0045 <xcao0045@student.monash.edu>
2025-07-08 16:14:29 +0800

    Create a large folder named "assessed lab" and save "week2" and "week3" respectively in this directory. This will facilitate submission and management.

---
+++ week3/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import JSONLab from './components/JSONLab.vue'
+import LoginForm2 from './components/LoginForm2.vue';
+</script>
+
+<template>
+  <!-- <JSONLab /> -->
+  <LoginForm2 />
+</template>
